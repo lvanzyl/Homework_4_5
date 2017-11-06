e5ec8b9d26d68efd970524496b37dfc3d32e101d
lvanzyl <lvanzyl@rams.colostate.edu>
2017-11-05 20:32:36 -0700

    starting functions

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -1,1 +1,21 @@
-perc_cis <- function()
+# x is number of drivers testing positive for drug, n is total number of
+#non-missing observations
+
+# first function
+perc_cis <- function(x, n) {
+  
+  p <- x/n
+  p_perc <- round(p * 100, digits = 1)
+  
+  se_p <- sqrt((p * (1-p))/n)
+  
+  upper_ci <- round(p_perc + (1.96 * se_p) * 100, digits = 1)
+  lower_ci <- round(p_perc - (1.96 * se_p) * 100, digits = 1)
+  
+  paste0(p_perc, "% (", lower_ci, "%, ", upper_ci, "%)")
+}
+
+# second function
+
+
+
